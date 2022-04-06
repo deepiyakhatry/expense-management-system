430c9fa111a84c5855203c14f9ce76335a9f7467
deepiya <deepkhatry7@gmail.com>
2022-04-06 21:55:28 +0545

    UI for login page

---
+++ src/component/login/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+
+return(
+    <div className="login">
+      <div className="login-header">
+        Login
+      </div>
+      <div className="">
+       NAME <input type="text" /><br/>
+       NAME <input type="text" />
+     </div>  
+    </div>
+)
+}
+
+export default Login
